738bd39aa784c4181f59c9bfa7f35a17ca8e321c
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-08-05 14:38:37 +0200

    Updated shellcheck

--- tools/build-helper.sh
+++ tools/build-helper.sh
@@ -21,7 +21,7 @@ fi
 case $1 in
     download-shellcheck)
         if [[ "${OSTYPE}" == linux* ]]; then
-            SHELLCHECK_VERSION="v0.4.6"
+            SHELLCHECK_VERSION="v0.4.7"
             SHELLCHECK_ARCHIVE="shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz"
             SHELLCHECK_ARCHIVE_SHA512SUM="d9ac3e4fb2383b2d6862415e8052459ce24fd5402806b9ce739990d5c1cccebe4121288df29de32dcef5daa115874ddf7f9730de256bf134ee11cd9704aaa64c"
             if [[ -x "${ROOT_DIR}/../build/shellcheck-${SHELLCHECK_VERSION}/shellcheck" ]]; then
